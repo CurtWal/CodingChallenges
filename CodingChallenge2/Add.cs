85cdf6bca4bfce32b8ed60ee0879046ed01ab291
curtrWal <curtrickw@yahoo.com>
2022-11-07 11:53:39 -0600

    Added challenge 2

---
+++ CodingChallenge2/Add.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallenge2
+{
+    internal class Add
+    {
+        public static void Added(int[] ray)
+        {
+            // adds the new vaule to the array
+            int[] NewArray = new int[ray.Length + 1];
+            int i;
+            for (i = 0; i < ray.Length; i++)
+            {
+                NewArray[i] = ray[i];
+            }
+            NewArray[i] = 10;
+            Console.WriteLine(string.Join(",", NewArray));
+
+            // built in method to remove
+            int item = 4;
+            ray = ray.Where(e => e != item).ToArray();
+            Console.WriteLine(String.Join(",", ray));
+
+            // written way
+            int j = 0;
+            int pos;
+            pos = 4;
+            // Shifting elements
+            for (j = pos - 1; j < 0; j++)
+            {
+                ray[j] = ray[j + 1];
+            }
+            Console.WriteLine(String.Join(",", ray));
+
+        }
+    }
+}
